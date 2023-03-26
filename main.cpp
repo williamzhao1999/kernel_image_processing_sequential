eec0a257c69d634825205de77297ffa836465dfe
William Zhao <william.zhao@stud.unifi.it>
2023-03-26 17:59:48 +0200

    Single test for 1 filter

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@ int main()
 
 
     //images.push_back("../images/road-7504719-4500.png");
-    //images.push_back("../images/road-7504719-6000.png");
+    images.push_back("../images/road-7504719-6000.png");
     //images.push_back("../images/road-7504719_1920.png");
     //images.push_back("../images/road-7504719_1280.png");
-    images.push_back("../images/road-7504719_640.png");
+    //images.push_back("../images/road-7504719_640.png");
     KernelImageProcessing kip;
 
     Matrix GaussianBlurFilter = kip.getGaussianBlurMatrix();
@@ -29,16 +29,26 @@ int main()
     std::vector<Matrix> filters {GaussianBlurFilter, BoxBlurFilter, SharpenFilter};
     const std::vector<std::string> filtersName {"GaussianBlur","BoxBlurFilter", "SharpenFilter"};
 
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
     for(int i = 0; i < images.size();i++){
         std::string filePath = images[i];
 
         cout << "Loading image..." << endl;
         Image image = kip.loadImage(filePath.c_str());
 
-        for(int j = 0; j < filters.size();j ++){
+        for(int j = 0; j < 1;j ++){
+
             std::cout << "Convoluting with "+filtersName[j]+" kernel matrix... " << std::endl;
+
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
             Image filteredImage = kip.convolute(image,filters[j],true);
+
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            int timeDifference = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+            std::cout << "Elapsed Time: " << timeDifference << std::endl;
+
             std::cout << "Convolution completed!" << std::endl;
 
             std::string targetFilePathName = targetPath + filtersName[j] + "_" + std::string(fs::path( filePath ).filename());
@@ -49,10 +59,7 @@ int main()
 
 
     }
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    int timeDifference = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
 
-    std::cout << "Elapsed Time: " << timeDifference << std::endl;
 
 
     return 0;
